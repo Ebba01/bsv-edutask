daf5a29545cbedb4fc0deb19c5a390d0c757831c
Ebba <ebbe24@student.bth.se>
2026-04-23 21:10:05 +0200

    remove files

--- backend/test/unit/test_hasAttribute_unit.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from src.util.helpers import hasAttribute
-
-@pytest.mark.unit
-def test_hasAttribute():
-    result = hasAttribute({'name': 'Jane'}, 'name')
-    assert result == True
